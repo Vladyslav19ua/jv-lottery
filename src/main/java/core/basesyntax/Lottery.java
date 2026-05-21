93c2c5c0e7b0a3ef00ad807db700d16bc32d4847
Vladyslav <Vlad19ua@gmail.com>
2026-05-22 00:19:49 +0300

    Fix Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_BALL_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 101;
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_BALL_NUMBER));
+        return new Ball(new ColorSupplier().getRandomColor(),new Random().nextInt(MAX_BALL_NUMBER));
     }
 }

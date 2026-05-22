372368ff8e41322dc2b0b791e5483b22ecdac885
Vladyslav <Vlad19ua@gmail.com>
2026-05-22 11:02:49 +0300

    Fix Lottery fieds

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 101;
+    private String colour;
+    int number;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(),new Random().nextInt(MAX_BALL_NUMBER));
+        colour = new ColorSupplier().getRandomColor();
+        number = new Random().nextInt(MAX_BALL_NUMBER);
+        return new Ball(colour,number);
     }
 }
